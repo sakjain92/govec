bd77c59cef307bb895ad37e9e6527cbfa6939dbd
Saksham Jain <sakjain92.work@gmail.com>
2017-11-19 23:07:25 -0500

    cprinter: Import printer files from go/printer
    go/printer is Go's AST printer for Go. We can use this (modified) printer and
    scanner to generate C equivalent code from Go.
    
    Signed-off-by: Saksham Jain <sakjain92.work@gmail.com>

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"go/token"
+	"go/parser"
+	"os"
+	"log"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	fset := token.NewFileSet()
+	node, err := parser.ParseFile(fset, "saxpy.go", nil, parser.ParseComments)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	/*
+	fo, err := os.Create("output.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	bufferedWriter := bufio.NewWriter(fo)
+	*/
+
+	err = Fprint(os.Stdout, fset, node)
+	if err != nil {
+	    log.Fatal(err)
+	}
 }
---
+++ saxpy.go
@@ -0,0 +1,14 @@
+package main
+
+func govecSaxpy(N int, alpha float32, X []float32, incX int, Y []float32, incY int) (int) {
+
+	var xi, yi int
+
+	for i := 0; i < N; i++ {
+		Y[yi] += alpha * X[xi]
+		xi += incX
+		yi += incY
+	}
+
+	return 0
+}
