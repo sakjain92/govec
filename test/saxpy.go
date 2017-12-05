c43cb370deea44176545d5dac3c85f927598ffe1
Saksham Jain <sakjain92.work@gmail.com>
2017-12-05 15:55:54 -0500

    Add support for linking object files with go
    Object code (compiled with -O3, statically linked) is comparable to C linking.
    
    TODO: Analyze why this is the case
    
    Benchmark results:
    
    Count is 1000, Using object linking
    TestSaxpy Passed
    BenchmarkCSaxpy-4         200000              9243 ns/op
    BenchmarkGoSaxpy-4        100000             15209 ns/op
    PASS
    ok      github.com/sakjain92/govec/test 3.624s
    
    Count is 1000, Using C linking
    TestSaxpy Passed
    BenchmarkCSaxpy-4         200000              9215 ns/op
    BenchmarkGoSaxpy-4        100000             15464 ns/op
    PASS
    ok      github.com/sakjain92/govec/test 3.641s
    
    Signed-off-by: Saksham Jain <sakjain92.work@gmail.com>

--- test/saxpy.go
+++ test/saxpy.go
@@ -1,7 +1,18 @@
 package main
 
+/*
 // #include "govec/saxpy.c"
 import "C"
+*/
+
+// #cgo CFLAGS: -Igovec
+// #cgo LDFLAGS: govec/libsaxpy.a
+// #include <saxpy.h>
+import "C"
+
+import (
+	"fmt"
+)
 
 func _govecSaxpy(N int, alpha float32, X []float32, incX int, Y []float32, incY int) {
 
@@ -17,3 +28,31 @@ func _govecSaxpy(N int, alpha float32, X []float32, incX int, Y []float32, incY 
 func saxpy(N int, alpha float32, X []float32, incX int, Y []float32, incY int) {
 	C.govecSaxpy(C.int(N), C.float(alpha), (*C.float)(&X[0]), C.int(incX), (*C.float)(&Y[0]), C.int(incY))
 }
+
+func main() {
+
+        var alpha float32
+        f1 := []float32{}
+        f2 := []float32{}
+        res := []float32{}
+
+        alpha = 2.0
+
+        for i := 0; i < 8; i++ {
+                f1 = append(f1, float32(i))
+                f2 = append(f2, float32(i * 2))
+                res = append(res, f2[i])
+        }
+
+        saxpy(8, alpha, f1, 1, res, 1)
+
+        for i := 0; i < 8; i++ {
+                expected := alpha * f1[i] + f2[i]
+                if res[i] != expected {
+                        panic("Results don't match")
+                }
+        }
+
+        fmt.Println("Main done")
+}
+
