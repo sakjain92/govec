efdfa93151743f93a51315f433043a81663941a2
Sivaprasad Sudhir <sivaprasad@Sivaprasads-MBP.wv.cc.cmu.edu>
2017-12-10 18:21:19 -0500

    Basic Mandelbrot interface

---
+++ blas/c/mandelbrot.c
@@ -0,0 +1,42 @@
+static inline int mandel(float c_re, float c_im, int count)
+{
+    float z_re = c_re, z_im = c_im;
+    int i;
+    for (i = 0; i < count; ++i) {
+
+        if (z_re * z_re + z_im * z_im > 4.f)
+            break;
+
+        float new_re = z_re*z_re - z_im*z_im;
+        float new_im = 2.f * z_re * z_im;
+        z_re = c_re + new_re;
+        z_im = c_im + new_im;
+    }
+
+    return i;
+}
+s
+
+void SerialMandelbrot(
+    float x0, float y0, float x1, float y1,
+    int width, int height,
+    int startRow, int totalRows,
+    int maxIterations,
+    int output[])
+{
+    float dx = (x1 - x0) / width;
+    float dy = (y1 - y0) / height;
+
+    int endRow = startRow + totalRows;
+
+    for (int j = startRow; j < endRow; j++) {
+        for (int i = 0; i < width; ++i) {
+            float x = x0 + i * dx;
+            float y = y0 + j * dy;
+
+            int index = (j * width + i);
+            output[index] = mandel(x, y, maxIterations);
+        }
+    }
+}
+
